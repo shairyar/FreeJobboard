b93b0eaeb79833019cb9a17a5ff7b3e326f02b2a
Shairyar Baig <shairyarbaig@gmail.com>
2019-03-30 15:21:27 +0500

    WIP: job posting with nested routes working

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,7 @@
 class Company < ApplicationRecord
   has_many :company_users
   has_many :users, through: :company_users
+
+  has_many :jobs
+  accepts_nested_attributes_for :jobs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,8 @@ class User < ApplicationRecord
   def is_employer?
     self.companies.count == 0 ? false : true
   end
+
+  def company
+    self.companies.first
+  end
 end
