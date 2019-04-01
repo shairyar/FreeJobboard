ac94391381bb26c5b3636c19bfc2e13cb4db8519
Shairyar Baig <shairyarbaig@gmail.com>
2019-04-01 13:29:57 +0500

    WIP - bullet initializer added and layout improvements for job and company

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,9 @@
+Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable = true        # enable Bullet gem, otherwise do nothing
+    Bullet.alert = true         # pop up a JavaScript alert in the browser
+    Bullet.bullet_logger = true # log to the Bullet log file (Rails.root/log/bullet.log)
+    Bullet.console = true       # log warnings to your browser's console.log (Safari/Webkit browsers or Firefox w/Firebug installed)
+    Bullet.rails_logger = true  # add warnings directly to the Rails log
+  end
+end
